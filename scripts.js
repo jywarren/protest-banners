85ff92b7f4564a64f5a03094d06a0c46d7bbb66f
Jeffrey Yoo Warren <jeff@unterbahn.com>
2020-06-28 13:18:34 -0400

    halftone-uploads

--- scripts.js
+++ scripts.js
@@ -85,9 +85,23 @@ function handleFile(e) {
   if (e.target.files.length > 0) {
     var file = p5.File._load(e.target.files[0], function(file) {
       if (file.type === 'image' || file.type.split('/')[0] === 'image') {
-        img = createImg(file.data, 'your uploaded pattern');
+
+        var img = createImg(file.data, 'your uploaded pattern');
         img.hide();
+
+        // build a canvas and try to use glfx to halftone it
+        loadImage(img.elt.src, function(i) {
+          var fxcanvas = fx.canvas(1024, 1024);
+          var texture = fxcanvas.texture(i.canvas);
+          fxcanvas.draw(texture)
+                  .denoise(80)
+                  .colorHalftone(320, 239.5, 0.31, 1.7)
+                  .update();
+          changeBackground(canvasToBlobUrl(fxcanvas));
+        });
+        // fallback in case halftoning doesn't work:
         changeBackground(img.elt.src);
+
       } else {
         img = null;
       }
@@ -119,16 +133,17 @@ function changeSlogan(t) {
     fill(textColor);
     textAlign(CENTER, CENTER);
 
-    canvas.drawingContext.globalAlpha = 0.7;
+//    canvas.drawingContext.globalAlpha = 0.7;
     text(t, width/2, height/2);
-    canvas.drawingContext.globalAlpha = 1;
-
+//    canvas.drawingContext.globalAlpha = 1;
+/*
 //    blendMode(SOFT_LIGHT);
 //    blendMode(OVERLAY);
     blendMode(SCREEN);
     text(t, width/2, height/2);
     blendMode(BLEND);
-  },0);
+ */
+ },0);
 }
 
 function changeSloganAndResetBackground(t) {
@@ -145,5 +160,33 @@ function changeBackground(src) {
     image(i, 0, 0, width, height);
     $('.preview').height($('.preview').width());
     changeSlogan(slogan)
-  });      
+  });
+}
+
+
+
+// http://stackoverflow.com/questions/4998908/convert-data-uri-to-file-then-append-to-formdata
+function dataURItoBlob(dataURI) {
+  // convert base64/URLEncoded data component to raw binary data held in a string
+  var byteString;
+  if (dataURI.split(',')[0].indexOf('base64') >= 0)
+      byteString = atob(dataURI.split(',')[1]);
+  else
+      byteString = unescape(dataURI.split(',')[1]);
+
+  // separate out the mime component
+  var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+  // write the bytes of the string to a typed array
+  var ia = new Uint8Array(byteString.length);
+  for (var i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+  }
+
+  return new Blob([ia], {type:mimeString});
+}
+
+function canvasToBlobUrl(canvas) {
+  var blob = dataURItoBlob(canvas.toDataURL('image/png'));
+  return window.URL.createObjectURL(blob);
 }
